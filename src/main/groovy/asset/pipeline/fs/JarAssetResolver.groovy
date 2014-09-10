0225844af0c3b14beabf30d7dc74edcae4e34bf0
David Estes <davydotcom@gmail.com>
2014-09-10 15:00:38 -0500

    fixing a typo

--- src/main/groovy/asset/pipeline/fs/JarAssetResolver.groovy
+++ src/main/groovy/asset/pipeline/fs/JarAssetResolver.groovy
@@ -88,7 +88,7 @@ class JarAssetResolver extends AbstractAssetResolver {
 		if(!basePath.startsWith('/') && relativeFile != null) {
 			def pathArgs = relativeFile.parentPath ? relativeFile.parentPath.split(DIRECTIVE_FILE_SEPARATOR) : [] //(path should be relative not canonical)
 			def basePathArgs = basePath.split(DIRECTIVE_FILE_SEPARATOR)
-			def parentPathArgs = pathArgs ? pathArgs[0..(pathArgs.size() - 2)] : []
+			def parentPathArgs = pathArgs ? pathArgs[0..(pathArgs.size() - 1)] : []
 			parentPathArgs.addAll(basePathArgs)
 			parentPathArgs = (parentPathArgs).findAll{it != "."}
 			basePath = parentPathArgs.join(File.separator)
