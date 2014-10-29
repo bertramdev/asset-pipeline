e0efeeda6f5e8e6c8eaa1685d23e8f35bdf1d56c
David Estes <davydotcom@gmail.com>
2014-10-29 09:34:05 -0400

    fixing abstract asset file

--- src/main/groovy/asset/pipeline/AbstractAssetFile.groovy
+++ src/main/groovy/asset/pipeline/AbstractAssetFile.groovy
@@ -66,7 +66,7 @@ abstract class AbstractAssetFile implements AssetFile {
 
 		def md5 = AssetHelper.getByteDigest(fileText.bytes)
 		if(!skipCache) {
-			def cache = CacheManager.findCache(canonicalPath, md5, baseFile?.path)
+			def cache = CacheManager.findCache(path, md5, baseFile?.path)
 			if(cache) {
 				return cache
 			}
