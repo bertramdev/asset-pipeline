a7322df3c9e422ed83a09531284cfd36a410623c
Ross Goldberg <rgoldberg@users.noreply.github.com>
2015-07-30 02:56:56 -0400

    Urls: added URL scheme Patterns & static method

--- asset-pipeline-core/src/main/groovy/asset/pipeline/utils/net/Urls.groovy
+++ asset-pipeline-core/src/main/groovy/asset/pipeline/utils/net/Urls.groovy
@@ -1,6 +1,7 @@
 package asset.pipeline.utils.net
 
 
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 
@@ -13,6 +14,16 @@ final class Urls {
      * URL starts with either {@code "/"} or a <a href="https://tools.ietf.org/html/std66">URI scheme</a> followed by {@code "/"}
      */
     static final Pattern ABSOLUTE_URL_PATTERN = ~/^(?:\p{L}[\p{L}\d+-.]*:)?\//
+
+    /**
+     * URL starts with a <a href="https://tools.ietf.org/html/std66">URI scheme</a>
+     */
+    static final Pattern URL_SCHEME_WITH_COLON_PATTERN = ~/^\p{L}[\p{L}\d+-.]*:/
+
+    /**
+     * URL starts with a <a href="https://tools.ietf.org/html/std66">URI scheme</a>
+     */
+    static final Pattern URL_SCHEME_SANS_COLON_PATTERN = ~/^\p{L}[\p{L}\d+-.]*(?=:)/
 
 
     /**
@@ -27,6 +38,18 @@ final class Urls {
      */
     static boolean isRelative(final String url) {
         ! isAbsolute(url)
+    }
+
+
+    /**
+     * @returns <a href="https://tools.ietf.org/html/std66">URI scheme</a>, including trailing colon, if present;
+     * otherwise, returns {@code defaultScheme}, which defaults to {@code null}
+     */
+    static String getSchemeWithColon(final String url, final String defaultScheme = null) {
+        final Matcher m = URL_SCHEME_WITH_COLON_PATTERN.matcher(url)
+        m.find() \
+            ? m.group()
+            : defaultScheme
     }
 
 
