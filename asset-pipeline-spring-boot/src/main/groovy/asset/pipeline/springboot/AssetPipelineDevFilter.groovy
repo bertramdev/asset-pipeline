25e86b7ac1651c51ea298d7fe1403ea755c48335
christian.oestreich <christian.oestreich@bluestembrands.com>
2015-09-23 11:23:05 -0500

    Adding mutable false to sort and code cleanup

--- asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineDevFilter.groovy
+++ asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineDevFilter.groovy
@@ -1,54 +1,54 @@
 package asset.pipeline.springboot
-import asset.pipeline.*
+
+import asset.pipeline.AssetPipeline
+import groovy.util.logging.Log4j
+import org.springframework.web.context.support.WebApplicationContextUtils
 
 import javax.servlet.*
-import org.springframework.web.context.support.WebApplicationContextUtils
-import groovy.util.logging.Log4j
 
 @Log4j
 class AssetPipelineDevFilter implements Filter {
-	def applicationContext
-	def servletContext
-	void init(FilterConfig config) throws ServletException {
-		applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
-		servletContext = config.servletContext
-		// permalinkService = applicationContext['spudPermalinkService']
-	}
+    def applicationContext
+    def servletContext
 
-	void destroy() {
-	}
+    void init(FilterConfig config) throws ServletException {
+        applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
+        servletContext = config.servletContext
+        // permalinkService = applicationContext['spudPermalinkService']
+    }
 
-	void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		def mapping = 'assets'
+    void destroy() {
+    }
 
-		def fileUri = request.requestURI
-		def baseAssetUrl = request.contextPath == "/" ? "/$mapping/" : "${request.contextPath}/${mapping}/"
-		if(fileUri.startsWith(baseAssetUrl)) {
-			fileUri = fileUri.substring(baseAssetUrl.length())
-		}
-		def format = servletContext.getMimeType(request.requestURI)
+    void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        def mapping = 'assets'
 
-		def fileContents = AssetPipeline.serveAsset(fileUri, format, null, request.characterEncoding)
+        def fileUri = request.requestURI
+        def baseAssetUrl = request.contextPath == "/" ? "/$mapping/" : "${request.contextPath}/${mapping}/"
+        if (fileUri.startsWith(baseAssetUrl)) {
+            fileUri = fileUri.substring(baseAssetUrl.length())
+        }
+        def format = servletContext.getMimeType(request.requestURI)
 
-		if (fileContents) {
+        def fileContents = AssetPipeline.serveAsset(fileUri, format, null, request.characterEncoding)
 
-			response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
-			response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
-			response.setDateHeader("Expires", 0); // Proxies.
-			response.setContentType(format)
-			try {
-				response.outputStream << fileContents
-				response.flushBuffer()
-				} catch(e) {
-					log.debug("File Transfer Aborted (Probably by the user)",e)
-				}
+        if (fileContents) {
 
+            response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            response.setDateHeader("Expires", 0); // Proxies.
+            response.setContentType(format)
+            try {
+                response.outputStream << fileContents
+                response.flushBuffer()
+            } catch (e) {
+                log.debug("File Transfer Aborted (Probably by the user)", e)
+            }
+        }
 
-		}
-
-			if (!response.committed) {
-				chain.doFilter(request, response)
-			}
-	}
+        if (!response.committed) {
+            chain.doFilter(request, response)
+        }
+    }
 
 }
