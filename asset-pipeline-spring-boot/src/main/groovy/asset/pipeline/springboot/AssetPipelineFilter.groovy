d1e9c9d3a531cde85abb10875093ee9e13a3bd45
christian.oestreich <christian.oestreich@bluestembrands.com>
2015-09-29 16:05:18 -0500

    Dates need to be in GMT format per http://www.w3.org/Protocols/rfc2616/rfc2616.html

--- asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineFilter.groovy
+++ asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineFilter.groovy
@@ -1,95 +1,110 @@
 package asset.pipeline.springboot
-import asset.pipeline.*
+
+import asset.pipeline.AssetPipelineConfigHolder
+import groovy.transform.CompileStatic
+import groovy.util.logging.Log4j
+import org.springframework.web.context.support.WebApplicationContextUtils
 
 import javax.servlet.*
-import org.springframework.web.context.support.WebApplicationContextUtils
-import groovy.util.logging.Log4j
+import java.text.SimpleDateFormat
 
 @Log4j
 class AssetPipelineFilter implements Filter {
     public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz"
     def applicationContext
-	def servletContext
-	void init(FilterConfig config) throws ServletException {
-		applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
-		servletContext = config.servletContext
-	}
+    def servletContext
 
-	void destroy() {
-	}
+    void init(FilterConfig config) throws ServletException {
+        applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
+        servletContext = config.servletContext
+    }
 
-	void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		def mapping = 'assets'
+    void destroy() {
+    }
 
-		def fileUri = request.requestURI
-		def baseAssetUrl = request.contextPath == "/" ? "/$mapping" : "${request.contextPath}/${mapping}"
-		if(fileUri.startsWith(baseAssetUrl)) {
-			fileUri = fileUri.substring(baseAssetUrl.length())
-		}
-		def file = applicationContext.getResource("classpath:assets${fileUri}")
-		if (file.exists()) {
+    void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        def mapping = 'assets'
+
+        def fileUri = request.requestURI
+        def baseAssetUrl = request.contextPath == "/" ? "/$mapping" : "${request.contextPath}/${mapping}"
+        if (fileUri.startsWith(baseAssetUrl)) {
+            fileUri = fileUri.substring(baseAssetUrl.length())
+        }
+        def file = applicationContext.getResource("classpath:assets${fileUri}")
+        if (file.exists()) {
             //Do this early so a 304 will still contain 'Last-Modified' in the case that there is a CDN in between client and server
-            response.setHeader('Last-Modified', new Date(file.lastModified()).format(HTTP_DATE_FORMAT))
-			if(checkETag(request, response, fileUri)) {
-				// Check for GZip
-				def acceptsEncoding = request.getHeader("Accept-Encoding")
-				if(acceptsEncoding?.split(",")?.contains("gzip")) {
-					def gzipFile = applicationContext.getResource("assets${fileUri}.gz")
-					if(gzipFile.exists()) {
-						file = gzipFile
-						response.setHeader('Content-Encoding','gzip')
-					}
-				}
-				def format = servletContext.getMimeType(request.requestURI)
-				def encoding = request.getCharacterEncoding()
-				if(encoding) {
-					response.setCharacterEncoding(encoding)
-				}
-				response.setContentType(format)
-				response.setHeader('Vary', 'Accept-Encoding')
-				response.setHeader('Cache-Control','public, max-age=31536000')
+            response.setHeader('Last-Modified', getLastModifiedDate(file))
+            if (checkETag(request, response, fileUri)) {
+                // Check for GZip
+                def acceptsEncoding = request.getHeader("Accept-Encoding")
+                if (acceptsEncoding?.split(",")?.contains("gzip")) {
+                    def gzipFile = applicationContext.getResource("assets${fileUri}.gz")
+                    if (gzipFile.exists()) {
+                        file = gzipFile
+                        response.setHeader('Content-Encoding', 'gzip')
+                    }
+                }
+                def format = servletContext.getMimeType(request.requestURI)
+                def encoding = request.getCharacterEncoding()
+                if (encoding) {
+                    response.setCharacterEncoding(encoding)
+                }
+                response.setContentType(format)
+                response.setHeader('Vary', 'Accept-Encoding')
+                response.setHeader('Cache-Control', 'public, max-age=31536000')
 
-				try {
-					response.outputStream << file.inputStream.getBytes()
-					response.flushBuffer()
-					} catch(e) {
-						log.debug("File Transfer Aborted (Probably by the user)",e)
-					}
-				}
+                try {
+                    response.outputStream << file.inputStream.getBytes()
+                    response.flushBuffer()
+                } catch (e) {
+                    log.debug("File Transfer Aborted (Probably by the user)", e)
+                }
+            }
 
-			}
+        }
 
-			if (!response.committed) {
-				chain.doFilter(request, response)
-			}
-		}
+        if (!response.committed) {
+            chain.doFilter(request, response)
+        }
+    }
 
-		/**
-		* Here we check if the request is contingent upon an ETag and if not, we append the ETag to the header key.
-		* This ETag is essentially the digested file name as it is unique unless the file changes.
-		* @return Whether processing should continue or not
-		*/
-		Boolean checkETag(ServletRequest request, ServletResponse response, fileUri) {
-			String etagName = getCurrentETag(fileUri)
+    /**
+     * Here we check if the request is contingent upon an ETag and if not, we append the ETag to the header key.
+     * This ETag is essentially the digested file name as it is unique unless the file changes.
+     * @return Whether processing should continue or not
+     */
+    Boolean checkETag(ServletRequest request, ServletResponse response, fileUri) {
+        String etagName = getCurrentETag(fileUri)
 
-			def ifNoneMatchHeader = request.getHeader('If-None-Match')
-			if(ifNoneMatchHeader && ifNoneMatchHeader == etagName) {
-				response.status = 304
-				response.flushBuffer()
-				return false
-			}
-			response.setHeader('ETag',etagName)
-			return true
-		}
+        def ifNoneMatchHeader = request.getHeader('If-None-Match')
+        if (ifNoneMatchHeader && ifNoneMatchHeader == etagName) {
+            response.status = 304
+            response.flushBuffer()
+            return false
+        }
+        response.setHeader('ETag', etagName)
+        return true
+    }
 
-		String getCurrentETag(String fileUri) {
-			def manifestPath = fileUri
-			if(fileUri.startsWith('/')) {
-				manifestPath = fileUri.substring(1) //Omit forward slash
-			}
+    String getCurrentETag(String fileUri) {
+        def manifestPath = fileUri
+        if (fileUri.startsWith('/')) {
+            manifestPath = fileUri.substring(1) //Omit forward slash
+        }
 
-			def manifest = AssetPipelineConfigHolder.manifest
+        def manifest = AssetPipelineConfigHolder.manifest
 
-			return manifest?.getProperty(manifestPath) ?: manifestPath
-		}
-	}
+        return manifest?.getProperty(manifestPath) ?: manifestPath
+    }
+
+    @CompileStatic
+    public static String getLastModifiedDate(File file) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(HTTP_DATE_FORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        Date currentTime = new Date()
+        if (file) {
+            currentTime = new Date(file.lastModified());
+        }
+        return sdf.format(currentTime)
+    }
+}
