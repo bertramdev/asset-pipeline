cb455f8236322306d64b70f2f5682e204e24631e
August Lilleaas <august@augustl.com>
2016-01-16 20:07:38 +0100

    Use new servlets from asset-pipeline-servlet

--- asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineFilter.groovy
+++ asset-pipeline-spring-boot/src/main/groovy/asset/pipeline/springboot/AssetPipelineFilter.groovy
@@ -2,7 +2,12 @@ package asset.pipeline.springboot
 
 import asset.pipeline.AssetPipelineConfigHolder
 import asset.pipeline.AssetPipelineResponseBuilder
+import asset.pipeline.servlet.AssetPipelineFilterCore
+import asset.pipeline.servlet.AssetPipelineServletResource
+import asset.pipeline.servlet.AssetPipelineServletResourceRepository
 import groovy.util.logging.Log4j
+import org.springframework.core.io.Resource
+import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.context.support.WebApplicationContextUtils
 
 import javax.servlet.*
@@ -10,129 +15,63 @@ import java.text.SimpleDateFormat
 
 @Log4j
 class AssetPipelineFilter implements Filter {
-    public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz"
-
-    def applicationContext
-    def servletContext
+    AssetPipelineFilterCore assetPipelineFilterCore = new AssetPipelineFilterCore()
 
     void init(FilterConfig config) throws ServletException {
+        assetPipelineFilterCore.servletContext = config.servletContext
+        assetPipelineFilterCore.mapping = "assets"
 
-        applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
-        servletContext = config.servletContext
+        WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.servletContext)
+        assetPipelineFilterCore.assetPipelineServletResourceRepository = new SpringServletResourceRepository(applicationContext)
     }
 
     void destroy() {
     }
 
     void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        def mapping = 'assets'
+        assetPipelineFilterCore.doFilter(request, response, chain)
+    }
 
-        def fileUri = request.requestURI
-        def baseAssetUrl = request.contextPath == "/" ? "/$mapping" : "${request.contextPath}/${mapping}"
-        if (fileUri.startsWith(baseAssetUrl)) {
-            fileUri = fileUri.substring(baseAssetUrl.length())
+    private static final class SpringServletResourceRepository implements AssetPipelineServletResourceRepository {
+        private final WebApplicationContext applicationContext
+        public SpringServletResourceRepository(WebApplicationContext applicationContext) {
+            this.applicationContext = applicationContext
         }
-        def file = applicationContext.getResource("classpath:assets${fileUri}")
-        if (file.exists()) {
-            //Do this early so a 304 will still contain 'Last-Modified' in the case that there is a CDN in between client and server
-            response.setHeader('Last-Modified', getLastModifiedDate(file))
-            if (checkETag(request, response, fileUri) && checkIfModifiedSince(request, file)) {
-                // Check for GZip
-                def acceptsEncoding = request.getHeader("Accept-Encoding")
-                if (acceptsEncoding?.split(",")?.contains("gzip")) {
-                    def gzipFile = applicationContext.getResource("assets${fileUri}.gz")
-                    if (gzipFile.exists()) {
-                        file = gzipFile
-                        response.setHeader('Content-Encoding', 'gzip')
-                    }
-                }
-                def format = servletContext.getMimeType(request.requestURI)
-                def encoding = request.getCharacterEncoding()
-                if (encoding) {
-                    response.setCharacterEncoding(encoding)
-                }
-                response.setContentType(format)
-                response.setHeader('Vary', 'Accept-Encoding')
-                response.setHeader('Cache-Control', 'public, max-age=31536000')
 
-                try {
-                    response.outputStream << file.inputStream.getBytes()
-                    response.flushBuffer()
-                } catch (e) {
-                    log.debug("File Transfer Aborted (Probably by the user)", e)
-                }
+        @Override
+        AssetPipelineServletResource getResource(String path) {
+            return SpringServletResource.create(applicationContext.getResource("classpath:assets${path}"))
+        }
+
+        @Override
+        AssetPipelineServletResource getGzippedResource(String path) {
+            return SpringServletResource.create(applicationContext.getResource("assets${path}.gz"))
+        }
+    }
+
+    private static final class SpringServletResource implements AssetPipelineServletResource {
+        private final Resource resource
+
+        private SpringServletResource(Resource resource) {
+            this.resource = resource
+        }
+
+        static SpringServletResource create(Resource resource) {
+            if (!resource.exists()) {
+                return null
             }
 
+            new SpringServletResource(resource)
         }
 
-        if (!response.committed) {
-            chain.doFilter(request, response)
-        }
-    }
-
-    /**
-     * Here we check if the request is contingent upon an ETag and if not, we append the ETag to the header key.
-     * This ETag is essentially the digested file name as it is unique unless the file changes.
-     * @return Whether processing should continue or not
-     */
-    Boolean checkETag(ServletRequest request, ServletResponse response, fileUri) {
-        String etagName = getCurrentETag(fileUri)
-
-        def ifNoneMatchHeader = request.getHeader('If-None-Match')
-        if (ifNoneMatchHeader && ifNoneMatchHeader == etagName) {
-            response.status = 304
-            response.flushBuffer()
-            return false
-        }
-        response.setHeader('ETag', "$etagName")
-        return true
-    }
-
-    Boolean checkIfModifiedSince(ServletRequest request, file) {
-        String ifNoneMatchHeader = request.getHeader('If-Modified-Since')
-        if (ifNoneMatchHeader && hasNotChanged(ifNoneMatchHeader, file)) {
-            response.status = 304
-            response.flushBuffer()
-            return false
-        }
-        return true
-    }
-
-    boolean hasNotChanged(String ifModifiedSince, file) {
-        SimpleDateFormat sdf = new SimpleDateFormat(HTTP_DATE_FORMAT);
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-        boolean hasNotChanged = false
-        if (ifModifiedSince) {
-            try {
-                hasNotChanged = new Date(file?.lastModified()) <= sdf.parse(ifModifiedSince)
-            } catch (Exception e) {
-                log.debug("Could not parse date time or file modified date", e)
-            }
-        }
-        return hasNotChanged
-    }
-
-    String getCurrentETag(String fileUri) {
-        def manifestPath = fileUri
-        if (fileUri.startsWith('/')) {
-            manifestPath = fileUri.substring(1) //Omit forward slash
+        @Override
+        Long getLastModified() {
+            return resource.lastModified()
         }
 
-        def manifest = AssetPipelineConfigHolder.manifest
-
-        return manifest?.getProperty(manifestPath) ?: manifestPath
-    }
-
-    private String getLastModifiedDate(file) {
-        SimpleDateFormat sdf = new SimpleDateFormat(HTTP_DATE_FORMAT);
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String lastModifiedDateTimeString = sdf.format(new Date())
-        try {
-            lastModifiedDateTimeString = sdf.format(new Date(file?.lastModified()))
-        } catch (Exception e) {
-            log.debug("Could not get last modified date time for file", e)
+        @Override
+        InputStream getInputStream() {
+            return resource.getInputStream()
         }
-
-        return lastModifiedDateTimeString
     }
 }
