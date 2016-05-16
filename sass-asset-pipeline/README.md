1e6b218417ae07b758bc169ea5b7bf2dbc59ee63
Gabriel Féron <feron.gabriel@gmail.com>
2016-05-16 16:29:44 +0200

    Import the replacement of sass-asset-pipeline from https://github.com/gferon/jsass-asset-pipeline

---
+++ sass-asset-pipeline/README.md
@@ -0,0 +1,23 @@
+SASS Asset Pipeline
+==========================
+The `jsass-asset-pipeline:5.1.0` is a plugin that provides SASS / SCSS support for the asset-pipeline static asset management plugin using [jsass](https://github.com/bit3/jsass), a Java wrapper around the native library  [libsass](https://github.com/sass/libsass) - the future of Sass compilation.
+
+The version numbering is synced on [jsass](https://github.com/bit3/jsass) versions, the underlying library used by this plugin.
+
+For more information on how to use asset-pipeline, visit [here](http://www.github.com/bertramdev/asset-pipeline).
+
+Configuration
+-------------
+
+For now, configuration is not really supported.
+
+Usage
+-----
+Simply create `scss` or `sass` files in your assets folder. 
+
+
+Things to be done
+-----------------
+
+* Add configuration options
+* Find a way to generate proper source maps with full paths
---
+++ sass-asset-pipeline/src/test/groovy/asset/pipeline/jsass/SassProcessorSpec.groovy
@@ -0,0 +1,83 @@
+/*
+* Copyright 2014 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package asset.pipeline.jsass
+
+import spock.lang.Specification
+import asset.pipeline.fs.*
+import asset.pipeline.*
+
+/**
+* @author David Estes
+*/
+class SassProcessorSpec extends Specification {
+
+	void "should compile sass into css"() {
+		given:
+		AssetPipelineConfigHolder.resolvers = []
+		AssetPipelineConfigHolder.config = [:]
+		AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver('test','assets'))
+		def assetFile = AssetHelper.fileForFullName('test.scss')
+		def processor = new SassProcessor()
+		when:
+		def output = processor.process(assetFile.inputStream.text,assetFile)
+		println "Results \n ${output}"
+		then:
+		output.contains('margin')
+	}
+
+	void "should compile nested sass into css"() {
+		given:
+		AssetPipelineConfigHolder.resolvers = []
+		AssetPipelineConfigHolder.config = [:]
+		AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver('test','assets'))
+		def assetFile = AssetHelper.fileForFullName('partials/forms.scss')
+		def processor = new SassProcessor()
+		when:
+		def output = processor.process(assetFile.inputStream.text,assetFile)
+		println "Results \n ${output}"
+		then:
+		output.contains('.sub')
+	}
+
+	void "should compile Bourbon"() {
+		given:
+		AssetPipelineConfigHolder.resolvers = []
+		AssetPipelineConfigHolder.config = [:]
+		AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver('test', 'assets'))
+		def assetFile = AssetHelper.fileForFullName('bourbon-test.scss')
+		def processor = new SassProcessor()
+		when:
+		def output = processor.process(assetFile.inputStream.text, assetFile)
+		println "Results \n ${output}"
+		then:
+		output.length() > 0
+	}
+
+	void "sould compile Bootstrap v4"() {
+		given:
+		AssetPipelineConfigHolder.resolvers = []
+		AssetPipelineConfigHolder.config = [:]
+		AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver('test', 'assets'))
+		def assetFile = AssetHelper.fileForFullName('bootstrap/bootstrap.scss')
+		def processor = new SassProcessor()
+		when:
+		def output = processor.process(assetFile.inputStream.text, assetFile)
+		println "Results \n ${output}"
+		then:
+		output.contains('Twitter')
+	}
+}
