48e677747ac1924c21571c0d6d6874bd180e888d
David Estes <davydotcom@gmail.com>
2020-04-28 11:38:40 -0400

    fixing test order run issue

--- asset-pipeline-core/src/test/groovy/asset/pipeline/processors/JsRequireProcessorSpec.groovy
+++ asset-pipeline-core/src/test/groovy/asset/pipeline/processors/JsRequireProcessorSpec.groovy
@@ -28,8 +28,9 @@ class JsRequireProcessorSpec extends Specification {
 	void "should be able to load another js file via commonJs syntax"() {
 		given:
 			def resolver = new FileSystemAssetResolver('application','assets')
+			AssetPipelineConfigHolder.config = [commonJs: true]
 		when:
-			def file = resolver.getAsset('asset-pipeline/test/test-common-js',null,'js')
+			def file = resolver.getAsset('asset-pipeline/test/test-common-js','application/javascript','js')
 			def processedText = file.processedStream(null)
 			println processedText
 		then:
