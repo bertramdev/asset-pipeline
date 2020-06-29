155c3984212e33433aaa866a8e34995b676e7cb4
David Estes <davydotcom@gmail.com>
2020-06-29 09:41:00 -0400

    fixing babel typo

--- asset-pipeline-core/src/main/groovy/asset/pipeline/processors/BabelJsProcessor.groovy
+++ asset-pipeline-core/src/main/groovy/asset/pipeline/processors/BabelJsProcessor.groovy
@@ -83,7 +83,7 @@ class BabelJsProcessor extends AbstractProcessor {
 			newEcmascriptKeywordsFound = true;
 		}
 		if(!newEcmascriptKeywordsFound && assetFile instanceof JsAssetFile) {
-			if((!newEcmascriptKeywordsFound && !AssetPipelineConfigHolder.config?.enableES6 && !AssetPipelineConfigHolder.config?."enable-es6") || (newEcmascriptKeywordsFound && (AssetPipelineConfigHolder.config?.enableES6 == false || AssetPipelineConfigHolder.config?."enable-es6" == false)))
+			if((!newEcmascriptKeywordsFound && !AssetPipelineConfigHolder.config?.enableES6 && !AssetPipelineConfigHolder.config?."enable-es6") || (newEcmascriptKeywordsFound && (AssetPipelineConfigHolder.config?.enableES6 == false || AssetPipelineConfigHolder.config?."enable-es6" == false))) {
 				return input
 			}
 		}
