b348e3f4ecbfee5f9efbf424b616e5e4c6d6cd1c
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-11-21 09:31:47 -0800

    org.grails.spring.config.http.GrailsFilters > org.grails.web.config.http.GrailsFilters

--- asset-pipeline-grails/src/main/groovy/asset/pipeline/AssetPipelineGrailsPlugin.groovy
+++ asset-pipeline-grails/src/main/groovy/asset/pipeline/AssetPipelineGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import asset.pipeline.fs.*
 import asset.pipeline.*
 import grails.util.BuildSettings
 import org.grails.plugins.BinaryGrailsPlugin
-import org.grails.spring.config.http.GrailsFilters
+import org.grails.web.config.http.GrailsFilters
 import groovy.util.logging.Commons
 import org.springframework.util.ClassUtils
 
